6e40c9b49397ec5d0d36e49a4d0eec68aa4367c3
VikaLevchyshyna <vika.levchyshyna@gmail.com>
2021-06-28 22:59:11 +0300

    hw10 done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
+'use strict';
 import './styles.css';
 import menu from './menu.json';
+import menuCardTpl from './templates/menu-cards.hbs'; 
+
+const refs = {
+    menuCardList: document.querySelector('.js-menu'),
+    containerThemeSwitch: document.querySelector('.theme-switch'),
+    toolbarThemeSwitch: document.querySelector('#theme-switch-toggle '),
+};
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+const cardsMarkup = createMenuCardsMarkup(menu);
+refs.menuCardList.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createMenuCardsMarkup(menu) {
+  return menuCardTpl(menu);
+}
+
+refs.toolbarThemeSwitch.addEventListener('change', onChangeThemeSwitch);
+getTheme();
+
+function onChangeThemeSwitch(evt) {
+  const darkTheme = document.body.classList.toggle(Theme.DARK);
+  localStorage.setItem('dark', darkTheme);
+}
+
+function getTheme() {
+  const keyTheme = localStorage.getItem('dark');
+  const strTheme = JSON.parse(keyTheme);
+
+  if (strTheme) {
+    refs.toolbarThemeSwitch.checked = true;
+    document.body.classList.add(Theme.DARK);
+  }
+}
